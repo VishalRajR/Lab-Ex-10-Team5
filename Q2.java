34db55a6aa40380bae39388f8a5b47f010d22629
VishalRajR <69119401+VishalRajR@users.noreply.github.com>
2020-10-06 20:23:40 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.ex10;
+import java.io.File; 
+/**
+ *
+ * @author vishal
+ */
+public class Q2 {
+    public static void main(String[] args) { 
+        //accept file name or directory name through command line args 
+        String fname =args[0]; 
+  
+        //pass the filename or directory name to File object 
+        File f = new File(fname); 
+  
+        //apply File class methods on File object 
+        System.out.println("File name :"+f.getName()); 
+        System.out.println("Path: "+f.getPath()); 
+        System.out.println("Absolute path:" +f.getAbsolutePath()); 
+        System.out.println("Parent:"+f.getParent()); 
+        System.out.println("Exists :"+f.exists()); 
+        if(f.exists()) 
+        { 
+            System.out.println("Is writeable:"+f.canWrite()); 
+            System.out.println("Is readable"+f.canRead()); 
+            System.out.println("Is a directory:"+f.isDirectory()); 
+            System.out.println("File Size in bytes "+f.length()); 
+        } 
+    } 
+    
+}
+
